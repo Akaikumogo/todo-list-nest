ac3e028484022a3a9c805ed86d4ebf62fb8d328b
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2024-07-30 15:46:43 +0500

    todo

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ITodo } from './interfaces/todo.interface';
 
 @Injectable()
 export class TodoService {
+  constructor(@InjectModel('Todo') private todoModel: Model<ITodo>) {}
   create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+    const newTodo = { ...createTodoDto };
+    const todo = new this.todoModel({
+      ...newTodo,
+      Status: 'OnPending',
+      addedDate: new Date(),
+    });
+    todo.save();
+    return { status: 201, message: 'Todo Created' };
   }
 
   findAll() {
