a7307ee3fca4049a24a755c1af94597efac963a1
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2024-08-03 17:33:36 +0500

    checktoken return user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UnauthorizedException } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller() // Bu qatorni qo'shing
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  private extractUser(req: Request): string {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      throw new UnauthorizedException('No token provided');
+    }
+    return JSON.parse(atob(authHeader.split(' ')[1]));
+  }
   @Get('')
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('CheckToken') // 'ChekcToken' ni 'CheckToken' ga o'zgartirdim
-  getCheckToken() {
-    return { status: 200, message: '' };
+  getCheckToken(@Req() req) {
+    return { status: 200, message: 'LOGINED', user: this.extractUser(req) };
   }
 }
